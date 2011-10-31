eb4f12f4091bd0acd00e5f66f53f9d5da4fbaf48
kandeen <>
2011-10-31 19:51:38 +0000

    modifications to FindTrims.cc from test beam

--- bin/FindTrims.cc
+++ bin/FindTrims.cc
@@ -12,11 +12,10 @@
 #include <string>
 #include <map>
 #include <cmath>
-#include <cstdlib>
 
 
 int const MINCOL = 13;
-int const MAXCOL = 39;
+int const MAXCOL = 38;
 int const MINROW = 39;
 int const MAXROW = 79;
 
@@ -61,19 +60,24 @@ int FindTrims (std::string const InFileName)
     //printf("%i %i %i %i %i %i %i %i %f\n" , mFec, mFecChannel, hubAddress, Col, Row, ROC, VCal, itrim, Efficiency);
     ROCID = 10000 * mFec + 1000 * mFecChannel + 10 * hubAddress + ROC;
     STrim[ROCID].Checked[Col - MINCOL][Row - MINROW] = true;
-    if (fabs(0.5 - Efficiency) < fabs(0.5 - STrim[ROCID].Eff[Col - MINCOL][Row - MINROW])) {
+    if (fabs(0.5 - Efficiency)*100 < fabs(50 - STrim[ROCID].Eff[Col - MINCOL][Row - MINROW])) {
+      if (Efficiency < 0.5){std::cout << "Efficiency = " << 100*Efficiency << " and STrim.Eff " << STrim[ROCID].Eff[Col - MINCOL][Row - MINROW]<< std::endl;}
       if (Col - MINCOL < 0 || Row - MINROW < 0) {
         std::cout << Col << "  " << Row << std::endl;
         std::cerr << "ERROR!!" << std::endl;
         exit(1);
       }
-      STrim[ROCID].Eff[Col - MINCOL][Row - MINROW]  = Efficiency;
+      STrim[ROCID].Eff[Col - MINCOL][Row - MINROW]  = 100*Efficiency;
       STrim[ROCID].Trim[Col - MINCOL][Row - MINROW] = itrim;
     }
   }
 
 
+
+
+
   for (std::map<int, B>::iterator It = STrim.begin(); It != STrim.end(); ++It) {
+    char BUFF_EFFIC[400];
     char BUFF[400];
     mFec        = It->first / 10000;
     mFecChannel = It->first % 10000 / 1000;
@@ -83,28 +87,36 @@ int FindTrims (std::string const InFileName)
 
     printf("%i  mFec = %i  mFecChannel = %i  hubAddress = %i  ROC = %i\n", It->first, mFec, mFecChannel, hubAddress, ROC);
 
+    sprintf(BUFF, "fasttrim_values_mFec%i_mFecChannel%i_hubAddress%i_roc%i.pix1", mFec, mFecChannel, hubAddress, ROC);
+    sprintf(BUFF_EFFIC, "fasttrim_effic_mFec%i_mFecChannel%i_hubAddress%i_roc%i.txt", mFec, mFecChannel, hubAddress, ROC);
 
-    sprintf(BUFF, "trimming_mFec%i_mFecChannel%i_hubAddress%i_roc%i.pix1", mFec, mFecChannel, hubAddress, ROC);
-    FILE* f = fopen(BUFF, "w");
-    if (!f) {
+
+    FILE* ftrim = fopen(BUFF, "w");
+    if (!ftrim) {
       std::cerr << "ERROR: cannot open output file: " << BUFF << std::endl;
       throw;
     }
-
-    fprintf(f, "5 0 0 0 0 0 0 0 0 0\n");
-    for (int icol = 0; icol != 27; ++icol) {
-      fprintf(f, "%i\n", icol <= 5 || icol >= 20 ? 0 : 1);
+    FILE* feff = fopen(BUFF_EFFIC, "w");
+    if (!feff) {
+      std::cerr << "ERROR: cannot open output file: " << BUFF_EFFIC << std::endl;
+      throw;
     }
 
+
+    fprintf(ftrim, "5 0 0 0 0 0 0 0 0 0\n");
+    for (int icol = 0; icol != 26; ++icol) {
+      fprintf(ftrim, "%i\n", icol <= 5 || icol >= 20 ? 0 : 1);
+    }
     for (int icol = 0; icol != NCOL; ++icol) {
       for (int irow = 0; irow != NROW; ++irow) {
-        fprintf(f, "%2i %2i 1 0 %2i\n", MINCOL + icol, MINROW + irow,
+        fprintf(ftrim, "%2i %2i 1 0 %2i\n", MINCOL + icol, MINROW + irow,
                 It->second.Checked[icol][irow] ? It->second.Trim[icol][irow] : 8);
-        //printf("%2i %2i %2i %9.5f\n", MINCOL + icol, MINROW + irow, It->second.Trim[icol][irow], It->second.Eff[icol][irow]);
+        fprintf(feff,"%2i %2i %2i %9.5f 50.0 %2i\n", ROC, MINCOL + icol, MINROW + irow, It->second.Eff[icol][irow], It->second.Trim[icol][irow]);
       }
     }
 
-    fclose(f);
+    fclose(ftrim);
+    fclose(feff);
   }
 
 
