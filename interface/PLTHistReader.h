7efdc549d335ec478256759d29d0f17af70c9f9e
dhidas <>
2012-04-17 15:06:45 +0000

    hihahi

---
+++ interface/PLTHistReader.h
@@ -0,0 +1,74 @@
+#ifndef GUARD_PLTHistReader_h
+#define GUARD_PLTHistReader_h
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+class PLTHistReader
+{
+  public:
+    PLTHistReader (std::string const);
+    ~PLTHistReader ();
+
+
+    int      GetNextBuffer ();
+    uint32_t GetOrbitTime ();
+    uint32_t GetOrbit ();
+    uint64_t GetTotal ();
+    uint64_t GetTotalInChannel (size_t const);
+    uint64_t GetTotalInBucket (size_t const);
+
+    int      AverageNext (int const);
+    uint64_t GetAverage ();
+    uint64_t GetAverageInChannel (size_t const);
+    uint64_t GetAverageInBucket (size_t const);
+    std::vector<uint32_t>* Channels ();
+    std::vector<uint32_t>* Buckets ();
+
+    static int const NBUCKETS = 3564;
+    static int const NMAXTELESCOPES = 48;
+
+  private:
+    std::ifstream fInFile;
+
+    int fAvgOver;
+    uint64_t fHist[NBUCKETS];
+
+    uint32_t fBigBuff[NMAXTELESCOPES][NBUCKETS];
+    uint32_t fOrbitTime[NMAXTELESCOPES];
+    uint32_t fOrbit[NMAXTELESCOPES];
+    
+    std::vector<uint32_t> fChannels;
+    std::vector<uint32_t> fBuckets;
+
+    uint32_t fAvgBigBuff[NMAXTELESCOPES][NBUCKETS];
+    uint32_t fAvgOrbitTime[NMAXTELESCOPES];
+    uint32_t fAvgOrbit[NMAXTELESCOPES];
+    std::vector<uint32_t> fAvgChannels;
+
+
+    // Temp internal scrap variables
+    uint32_t fTempOrbitTime;
+    uint32_t fTempOrbit;
+    uint32_t fTempChannel;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
---
+++ src/PLTHistReader.cc
@@ -0,0 +1,155 @@
+#include "PLTHistReader.h"
+
+PLTHistReader::PLTHistReader (std::string const InFileName)
+{
+  fInFile.open(InFileName.c_str(), std::ios::in | std::ios::binary);
+  if (!fInFile.is_open()) {
+    std::cerr << "ERROR: cannot open file: " << InFileName << std::endl;
+    throw;
+  }
+  
+  do {
+    GetNextBuffer();
+  } while (fTempOrbit < 25000);
+}
+
+
+PLTHistReader::~PLTHistReader ()
+{
+}
+
+
+
+
+std::vector<uint32_t>* PLTHistReader::Channels ()
+{
+  return &fChannels;
+}
+
+
+std::vector<uint32_t>* PLTHistReader::Buckets ()
+{
+  return &fBuckets;
+}
+
+
+int PLTHistReader::GetNextBuffer ()
+{
+  fChannels.clear();
+  fBuckets.clear();
+  static uint32_t LastOrbitTime = -1;
+  uint32_t MyOrbitTime;
+  if (LastOrbitTime == (uint32_t) -1) {
+    fInFile.read( (char*) &MyOrbitTime, sizeof(uint32_t));
+    fTempOrbitTime = MyOrbitTime;
+  }
+
+  if (fInFile.eof()) {
+    return -1;
+  }
+
+
+  //for (int i = 0; i != NMAXTELESCOPES; ++i) {
+  //  for (int j = 0; j != NBUCKETS; ++j) {
+  //    fBigBuff[i][j] = 0;
+  //  }
+  //  fOrbitTime[i] = 0;
+  //  fOrbit[i] = 0;
+  //}
+
+  do {
+    fInFile.read( (char*) &fTempOrbit, sizeof(uint32_t));
+    fInFile.read( (char*) &fTempChannel, sizeof(uint32_t));
+    fInFile.read( (char*) fBigBuff[fTempChannel], NBUCKETS * sizeof(uint32_t));
+
+    fOrbitTime[fTempChannel] = fTempOrbitTime;
+    fOrbit[fTempChannel] = fTempOrbit;
+    fChannels.push_back(fTempChannel);
+
+    for (uint32_t ib = 0; ib < NBUCKETS; ++ib) {
+      if ((fBigBuff[fTempChannel][ib] & 0xfff) != 0) {
+        fBuckets.push_back(ib);
+      }
+    }
+    std::sort(fBuckets.begin(), fBuckets.end());
+    std::unique(fBuckets.begin(), fBuckets.end());
+
+
+    if (fInFile.eof()) {
+      return -1;
+    }
+
+    fInFile.read( (char*) &fTempOrbitTime, sizeof(uint32_t));
+
+  } while (fTempOrbitTime == MyOrbitTime);
+
+  LastOrbitTime = fTempOrbitTime;
+  MyOrbitTime = LastOrbitTime;
+
+  return (int) fChannels.size();
+}
+
+
+uint32_t PLTHistReader::GetOrbitTime ()
+{
+  return fOrbitTime[fChannels[0]];
+}
+
+uint32_t PLTHistReader::GetOrbit ()
+{
+  return fOrbit[fChannels[0]];
+}
+
+
+
+int PLTHistReader::AverageNext (int const NToAvg)
+{
+  for (int i = 0; i < NToAvg; ++i) {
+    if (GetNextBuffer() == -1) {
+      return -1;
+    }
+
+    for (std::vector<uint32_t>::iterator ich = fChannels.begin(); ich != fChannels.end(); ++ich) {
+      //fAvgHist[*ich];
+    }
+  }
+
+  return 0;
+}
+
+
+
+
+
+uint64_t PLTHistReader::GetTotal ()
+{
+  uint64_t Sum = 0;
+  for (size_t ich = 0; ich != fChannels.size(); ++ich) {
+    for (int i = 0; i != NBUCKETS; ++i) {
+      Sum += (fBigBuff[ich][i] & 0xfff);
+    }
+  }
+
+  return Sum;
+}
+
+uint64_t PLTHistReader::GetTotalInChannel (size_t const Channel)
+{
+  uint64_t Sum = 0;
+  for (int i = 0; i != NBUCKETS; ++i) {
+    Sum += (fBigBuff[Channel][i] & 0xfff);
+  }
+
+  return Sum;
+}
+
+uint64_t PLTHistReader::GetTotalInBucket (size_t const Bucket)
+{
+  uint64_t Sum = 0;
+  for (size_t ich = 0; ich != fChannels.size(); ++ich) {
+    Sum += (fBigBuff[ich][Bucket] & 0xfff);
+  }
+
+  return Sum;
+}
+
