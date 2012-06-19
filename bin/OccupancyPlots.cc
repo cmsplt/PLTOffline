78ac2909fbc0c060a064e42c052601f479854e8e
dhidas <>
2012-06-19 14:33:39 +0000

    _All for occ

--- bin/OccupancyPlots.cc
+++ bin/OccupancyPlots.cc
@@ -51,8 +51,8 @@ TH1F* FidHistFrom2D (TH2F* hIN, TString const NewName, int const NBins, PLTPlane
 
   for (int ix = 1; ix <= NBinsX; ++ix) {
     for (int iy = 1; iy <= NBinsY; ++iy) {
-      int const px = hIN->GetXaxis()->GetBinLowEdge(ix);
-      int const py = hIN->GetYaxis()->GetBinLowEdge(iy);
+      int const px = (int) hIN->GetXaxis()->GetBinLowEdge(ix);
+      int const py = (int) hIN->GetYaxis()->GetBinLowEdge(iy);
       if (PLTPlane::IsFiducial(FidRegion, px, py)) {
         if (hIN->GetBinContent(ix, iy) > ZMax) {
           h->Fill(ZMax - hIN->GetMaximum() / (float) NBins);
@@ -75,7 +75,7 @@ TH2F* Get3x3EfficiencyHist (TH2F& HistIn, int const FirstCol, int const LastCol,
   for (int icol = 1; icol <= HistIn.GetNbinsX(); ++icol) {
 
     // What pixel column is this?  If it's outside the range skip it
-    int const PixCol = HistEff->GetXaxis()->GetBinLowEdge(icol);
+    int const PixCol = (int) HistEff->GetXaxis()->GetBinLowEdge(icol);
     if (PixCol < FirstCol || PixCol > LastCol) {
       continue;
     }
@@ -83,7 +83,7 @@ TH2F* Get3x3EfficiencyHist (TH2F& HistIn, int const FirstCol, int const LastCol,
     for (int irow = 1; irow <= HistIn.GetNbinsY(); ++irow) {
 
       // What row is this?  If it's outside the range skip it
-      int const PixRow = HistEff->GetYaxis()->GetBinLowEdge(irow);
+      int const PixRow = (int) HistEff->GetYaxis()->GetBinLowEdge(irow);
       if (PixRow < FirstRow || PixRow > LastRow) {
         continue;
       }
@@ -134,17 +134,18 @@ int OccupancyPlots (std::string const DataFileName)
   // Grab the plt event reader
   PLTEvent Event(DataFileName);
   //Event.SetPlaneClustering(PLTPlane::kClustering_NoClustering);
-  PLTPlane::FiducialRegion MyFiducialRegion = PLTPlane::kFiducialRegion_m1_m1;
+  PLTPlane::FiducialRegion MyFiducialRegion = PLTPlane::kFiducialRegion_All;
   //  Event.SetPlaneClustering(PLTPlane::kClustering_AllTouching);
-  Event.SetPlaneClustering(PLTPlane::kClustering_AllTouching, PLTPlane::kFiducialRegion_m1_m1);
+  Event.SetPlaneClustering(PLTPlane::kClustering_AllTouching, PLTPlane::kFiducialRegion_All);
   Event.SetPlaneFiducialRegion(MyFiducialRegion);
   Event.SetTrackingAlgorithm(PLTTracking::kTrackingAlgorithm_NoTracking);
 
   // Map for all ROC hists and canvas
-  std::map<int, TH2F*>    hAllMap;
   std::map<int, TCanvas*> cAllMap;
   std::map<int, TH2F*>    hOccupancyMap;
   std::map<int, TCanvas*> cOccupancyMap;
+  std::map<int, std::vector<TH2F*> >    hOccupancyClMap;
+  std::map<int, TCanvas*> cOccupancyClMap;
   std::map<int, TH2F*>    hQuantileMap;
   std::map<int, TCanvas*> cQuantileMap;
   std::map<int, TCanvas*> cProjectionMap;
@@ -167,6 +168,9 @@ int OccupancyPlots (std::string const DataFileName)
       std::cout << "Processing event: " << ientry << std::endl;
     }
 
+
+    //if (ientry == 300000) break;
+
     // Loop over all planes with hits in event
     for (size_t ip = 0; ip != Event.NPlanes(); ++ip) {
 
@@ -186,72 +190,112 @@ int OccupancyPlots (std::string const DataFileName)
       }
 
 
-      // Loop over all hits on this plane
-      for (size_t ihit = 0; ihit != Plane->NHits(); ++ihit) {
+      for (size_t icl = 0; icl != Plane->NClusters(); ++icl) {
+        PLTCluster* Cluster = Plane->Cluster(icl);
 
-        // THIS hit is
-        PLTHit* Hit = Plane->Hit(ihit);
-        //printf("Channel ROC Row Col ADC: %2i %1i %2i %2i %4i %12i\n", Hit->Channel(), Hit->ROC(), Hit->Row(), Hit->Column(), Hit->ADC(), Event.EventNumber());
+        // Loop over all hits on this plane
+        for (size_t ihit = 0; ihit != Cluster->NHits(); ++ihit) {
 
-        // ID the plane and roc by 3 digit number
-        int const id = 10 * Plane->Channel() + Plane->ROC();
+          // THIS hit is
+          PLTHit* Hit = Cluster->Hit(ihit);
+          //printf("Channel ROC Row Col ADC: %2i %1i %2i %2i %4i %12i\n", Hit->Channel(), Hit->ROC(), Hit->Row(), Hit->Column(), Hit->ADC(), Event.EventNumber());
 
-        // If the hist doesn't exist yet we have to make it
-        if (hOccupancyMap.count(id) == 0) {
+          // ID the plane and roc by 3 digit number
+          int const id = 10 * Plane->Channel() + Plane->ROC();
 
-          // Create new hist with the given name
-          sprintf(BUFF, "Occupancy Ch%02i ROC%1i", Plane->Channel(), Plane->ROC());
-          hOccupancyMap[id] = new TH2F(BUFF, BUFF, PLTU::NCOL, PLTU::FIRSTCOL, PLTU::LASTCOL+1, PLTU::NROW,PLTU::FIRSTROW, PLTU::LASTROW+1);
-          hOccupancyMap[id]->SetXTitle("Column");
-          hOccupancyMap[id]->SetYTitle("Row");
-          hOccupancyMap[id]->SetZTitle("Number of Hits");
-          hOccupancyMap[id]->GetXaxis()->CenterTitle();
-          hOccupancyMap[id]->GetYaxis()->CenterTitle();
-          hOccupancyMap[id]->GetZaxis()->CenterTitle();
-          hOccupancyMap[id]->SetTitleOffset(1.2, "y");
-          hOccupancyMap[id]->SetTitleOffset(1.4, "z");
-          hOccupancyMap[id]->SetFillColor(40); // We need this for projections later
-          hOccupancyMap[id]->SetStats(false);
+          // If the hist doesn't exist yet we have to make it
+          if (hOccupancyMap.count(id) == 0) {
+
+            // Create new hist with the given name
+            sprintf(BUFF, "Occupancy Ch%02i ROC%1i", Plane->Channel(), Plane->ROC());
+            hOccupancyMap[id] = new TH2F(BUFF, BUFF, PLTU::NCOL, PLTU::FIRSTCOL, PLTU::LASTCOL+1, PLTU::NROW,PLTU::FIRSTROW, PLTU::LASTROW+1);
+            hOccupancyMap[id]->SetXTitle("Column");
+            hOccupancyMap[id]->SetYTitle("Row");
+            hOccupancyMap[id]->SetZTitle("Number of Hits");
+            hOccupancyMap[id]->GetXaxis()->CenterTitle();
+            hOccupancyMap[id]->GetYaxis()->CenterTitle();
+            hOccupancyMap[id]->GetZaxis()->CenterTitle();
+            hOccupancyMap[id]->SetTitleOffset(1.2, "y");
+            hOccupancyMap[id]->SetTitleOffset(1.4, "z");
+            hOccupancyMap[id]->SetFillColor(40); // We need this for projections later
+            hOccupancyMap[id]->SetStats(false);
+
+            for (int icl = 0; icl != 3; ++icl) {
+              sprintf(BUFF, "Occupancy Cluster npix=%i Ch%02i ROC%1i", icl + 1, Plane->Channel(), Plane->ROC());
+              hOccupancyClMap[id][icl] = new TH2F(BUFF, BUFF, PLTU::NCOL, PLTU::FIRSTCOL, PLTU::LASTCOL+1, PLTU::NROW,PLTU::FIRSTROW, PLTU::LASTROW+1);
+              hOccupancyClMap[id][icl]->SetXTitle("Column");
+              hOccupancyClMap[id][icl]->SetYTitle("Row");
+              hOccupancyClMap[id][icl]->SetZTitle("Number of Hits");
+              hOccupancyClMap[id][icl]->GetXaxis()->CenterTitle();
+              hOccupancyClMap[id][icl]->GetYaxis()->CenterTitle();
+              hOccupancyClMap[id][icl]->GetZaxis()->CenterTitle();
+              hOccupancyClMap[id][icl]->SetTitleOffset(1.2, "y");
+              hOccupancyClMap[id][icl]->SetTitleOffset(1.4, "z");
+              hOccupancyClMap[id][icl]->SetFillColor(40); // We need this for projections later
+              hOccupancyClMap[id][icl]->SetStats(false);
+            }
 
 
 
+            // If we're making a new hist I'd say there's a 1 in 3 chance we'll need a canvas for it
+            if (!cOccupancyMap.count(Plane->Channel())) {
 
-          // If we're making a new hist I'd say there's a 1 in 3 chance we'll need a canvas for it
-          if (!cOccupancyMap.count(Plane->Channel())) {
+              // Create canvas with given name
+              sprintf(BUFF, "Occupancy All Ch%02i", Plane->Channel());
+              cAllMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 1200, 1200);
+              cAllMap[Plane->Channel()]->Divide(3,3);
 
-            // Create canvas with given name
-            sprintf(BUFF, "Occupancy All Ch%02i", Plane->Channel());
-            cAllMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 1200, 1200);
-            cAllMap[Plane->Channel()]->Divide(3,3);
+              sprintf(BUFF, "Occupancy Ch%02i", Plane->Channel());
+              cOccupancyMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 1200, 1200);
+              cOccupancyMap[Plane->Channel()]->Divide(3,3);
 
-            sprintf(BUFF, "Occupancy Ch%02i", Plane->Channel());
-            cOccupancyMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 1200, 1200);
-            cOccupancyMap[Plane->Channel()]->Divide(3,3);
+              sprintf(BUFF, "Occupancy Cluster_Ch%02i", Plane->Channel());
+              cOccupancyClMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 1200, 1200);
+              cOccupancyClMap[Plane->Channel()]->Divide(3,3);
 
-            sprintf(BUFF, "Occupancy w/ QuantilesCh%02i", Plane->Channel());
-            cQuantileMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 1200, 800);
-            cQuantileMap[Plane->Channel()]->Divide(3,2);
+              sprintf(BUFF, "Occupancy w/ QuantilesCh%02i", Plane->Channel());
+              cQuantileMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 1200, 800);
+              cQuantileMap[Plane->Channel()]->Divide(3,2);
 
-            sprintf(BUFF, "Occupancy Projection Ch%02i", Plane->Channel());
-            cProjectionMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 1200, 1200);
-            cProjectionMap[Plane->Channel()]->Divide(3,3);
+              sprintf(BUFF, "Occupancy Projection Ch%02i", Plane->Channel());
+              cProjectionMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 1200, 1200);
+              cProjectionMap[Plane->Channel()]->Divide(3,3);
 
-            sprintf(BUFF, "Occupancy Efficiency Ch%02i", Plane->Channel());
-            cEfficiencyMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 1200, 1200);
-            cEfficiencyMap[Plane->Channel()]->Divide(3,3);
+              sprintf(BUFF, "Occupancy Efficiency Ch%02i", Plane->Channel());
+              cEfficiencyMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 1200, 1200);
+              cEfficiencyMap[Plane->Channel()]->Divide(3,3);
 
-            sprintf(BUFF, "Planes hit in Ch%02i", Plane->Channel());
-            cCoincidenceMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 400, 400);
-            hCoincidenceMap[Plane->Channel()] = new TH1F(BUFF, BUFF, 7, 0, 7);
+              sprintf(BUFF, "Planes hit in Ch%02i", Plane->Channel());
+              cCoincidenceMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 400, 400);
+              hCoincidenceMap[Plane->Channel()] = new TH1F(BUFF, BUFF, 7, 0, 7);
 
-            sprintf(BUFF, "Occupancy by Mean Ch%02i", Plane->Channel());
-            cMeanMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 1200, 800);
-            cMeanMap[Plane->Channel()]->Divide(3,2);
+              sprintf(BUFF, "Occupancy by Mean Ch%02i", Plane->Channel());
+              cMeanMap[Plane->Channel()] = new TCanvas(BUFF, BUFF, 1200, 800);
+              cMeanMap[Plane->Channel()]->Divide(3,2);
+            }
           }
+
+          // Fill this histogram with the given id
+          hOccupancyMap[id]->Fill(Hit->Column(), Hit->Row());
         }
 
-        // Fill this histogram with the given id
-        hOccupancyMap[id]->Fill(Hit->Column(), Hit->Row());
+
+        int const id = Cluster->Channel() * 10 + Cluster->ROC();
+        size_t NHits = Cluster->NHits();
+        if (NHits == 1) {
+          Cluster->Hit(0)->Column();
+          Cluster->Hit(0)->Row();
+          hOccupancyClMap[id][0]->Fill(12,12);
+          hOccupancyClMap[id][0]->Fill(Cluster->Hit(0)->Column(), Cluster->Hit(0)->Row());
+        } else if (NHits == 2) {
+          for (size_t ihit = 0; ihit != NHits; ++ihit) {
+            hOccupancyClMap[id][1]->Fill(Cluster->Hit(ihit)->Column(), Cluster->Hit(ihit)->Row());
+          }
+        } else if (NHits >= 3 ) {
+          for (size_t ihit = 0; ihit != NHits; ++ihit) {
+            hOccupancyClMap[id][2]->Fill(Cluster->Hit(ihit)->Column(), Cluster->Hit(ihit)->Row());
+          }
+        }
       }
     }
 
@@ -277,6 +321,22 @@ int OccupancyPlots (std::string const DataFileName)
   }
 
   std::cout << "Done reading events.  Will make some plots now" << std::endl;
+
+  // Loop over all histograms and draw them on the correct canvas in the correct pad
+  for (std::map<int, std::vector<TH2F*> >::iterator it = hOccupancyClMap.begin(); it != hOccupancyClMap.end(); ++it) {
+    // Decode the ID
+    int const Channel = it->first / 10;
+    int const ROC     = it->first % 10;
+    int const id      = it->first;
+
+    cOccupancyClMap[Channel]->cd(ROC+1);
+    hOccupancyClMap[id][0]->Draw("colz");
+    cOccupancyClMap[Channel]->cd(ROC+3+1);
+    hOccupancyClMap[id][1]->Draw("colz");
+    cOccupancyClMap[Channel]->cd(ROC+6+1);
+    hOccupancyClMap[id][2]->Draw("colz");
+
+  }
 
   // Loop over all histograms and draw them on the correct canvas in the correct pad
   for (std::map<int, TH2F*>::iterator it = hOccupancyMap.begin(); it != hOccupancyMap.end(); ++it) {
@@ -438,33 +498,38 @@ int OccupancyPlots (std::string const DataFileName)
     it->second->GetYaxis()->SetTitleOffset(1.9);
     it->second->GetYaxis()->CenterTitle();
     it->second->Draw("");
+
   }
 
   // Loop over all canvas, save them, and delete them
   for (std::map<int, TCanvas*>::iterator it = cOccupancyMap.begin(); it != cOccupancyMap.end(); ++it) {
-    it->second->SaveAs(TString::Format("plots/Occupancy_Ch%02i.png", it->first));
+    it->second->SaveAs(TString::Format("plots/Occupancy_Ch%02i.gif", it->first));
+    delete it->second;
+  }
+  for (std::map<int, TCanvas*>::iterator it = cOccupancyClMap.begin(); it != cOccupancyClMap.end(); ++it) {
+    it->second->SaveAs(TString::Format("plots/Occupancy_Clusters_Ch%02i.gif", it->first));
     delete it->second;
   }
   for (std::map<int, TCanvas*>::iterator it = cQuantileMap.begin(); it != cQuantileMap.end(); ++it) {
-    it->second->SaveAs(TString::Format("plots/Occupancy_Quantile_Ch%02i.png", it->first));
+    it->second->SaveAs(TString::Format("plots/Occupancy_Quantile_Ch%02i.gif", it->first));
     delete it->second;
   }
   for (std::map<int, TCanvas*>::iterator it = cProjectionMap.begin(); it != cProjectionMap.end(); ++it) {
-    it->second->SaveAs(TString::Format("plots/Occupancy_Projection_Ch%02i.png", it->first));
+    it->second->SaveAs(TString::Format("plots/Occupancy_Projection_Ch%02i.gif", it->first));
     delete it->second;
   }
   for (std::map<int, TCanvas*>::iterator it = cEfficiencyMap.begin(); it != cEfficiencyMap.end(); ++it) {
-    it->second->SaveAs(TString::Format("plots/Occupancy_Efficiency_Ch%02i.png", it->first));
+    it->second->SaveAs(TString::Format("plots/Occupancy_Efficiency_Ch%02i.gif", it->first));
     delete it->second;
   }
   for (std::map<int, TCanvas*>::iterator it = cCoincidenceMap.begin(); it != cCoincidenceMap.end(); ++it) {
-    it->second->SaveAs(TString::Format("plots/Occupancy_Coincidence_Ch%02i.png", it->first));
+    it->second->SaveAs(TString::Format("plots/Occupancy_Coincidence_Ch%02i.gif", it->first));
   }
   for (std::map<int, TCanvas*>::iterator it = cMeanMap.begin(); it != cMeanMap.end(); ++it) {
-    it->second->SaveAs(TString::Format("plots/Occupancy_Mean_Ch%02i.png", it->first));
+    it->second->SaveAs(TString::Format("plots/Occupancy_Mean_Ch%02i.gif", it->first));
   }
   for (std::map<int, TCanvas*>::iterator it = cAllMap.begin(); it != cAllMap.end(); ++it) {
-    it->second->SaveAs(TString::Format("plots/Occupancy_All_Ch%02i.png", it->first));
+    it->second->SaveAs(TString::Format("plots/Occupancy_All_Ch%02i.gif", it->first));
     delete it->second;
   }
 
