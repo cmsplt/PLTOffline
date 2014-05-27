b72fc9ccf718b46ddfc86a83c0cc0f3216a84216
Gregor Kasieczka <g.kasieczka@gmail.com>
2014-05-27 12:49:52 +0200

    update for 6 planes in tracking

--- src/PSIBinaryFileReader.cc
+++ src/PSIBinaryFileReader.cc
@@ -40,12 +40,12 @@ PSIBinaryFileReader::PSIBinaryFileReader (std::string const InFileName, std::str
   //fGainCal.ReadGainCalFile("Telescope_Recalib_09052014_1718pm/phCalibration_C3.dat.fit.dat");
   //fGainCal.ReadGainCalFile("Telescope_Recalib_09052014_1718pm/phCalibration_C4.dat.fit.dat");
   //fGainCal.ReadGainCalFile("Telescope_Recalib_09052014_1718pm/phCalibration_C5.dat.fit.dat");
-  fGainCal.ReadGainCalFile("/Users/dhidas/Telescope_NewBoards_18052014_1418/phCalibration_C0.dat.fit.dat");
-  fGainCal.ReadGainCalFile("/Users/dhidas/Telescope_NewBoards_18052014_1418/phCalibration_C1.dat.fit.dat");
-  fGainCal.ReadGainCalFile("/Users/dhidas/Telescope_NewBoards_18052014_1418/phCalibration_C2.dat.fit.dat");
-  fGainCal.ReadGainCalFile("/Users/dhidas/Telescope_NewBoards_18052014_1418/phCalibration_C3.dat.fit.dat");
-  fGainCal.ReadGainCalFile("/Users/dhidas/Telescope_NewBoards_18052014_1418/phCalibration_C4.dat.fit.dat");
-  fGainCal.ReadGainCalFile("/Users/dhidas/Telescope_NewBoards_18052014_1418/phCalibration_C5.dat.fit.dat");
+  fGainCal.ReadGainCalFile("Telescope_NewBoards_27052014_0145/phCalibration_C0.dat.fit.dat");
+  fGainCal.ReadGainCalFile("Telescope_NewBoards_27052014_0145/phCalibration_C1.dat.fit.dat");
+  fGainCal.ReadGainCalFile("Telescope_NewBoards_27052014_0145/phCalibration_C2.dat.fit.dat");
+  fGainCal.ReadGainCalFile("Telescope_NewBoards_27052014_0145/phCalibration_C3.dat.fit.dat");
+  fGainCal.ReadGainCalFile("Telescope_NewBoards_27052014_0145/phCalibration_C4.dat.fit.dat");
+  fGainCal.ReadGainCalFile("Telescope_NewBoards_27052014_0145/phCalibration_C5.dat.fit.dat");
   //fGainCal.ReadGainCalFileExt("/Users/dhidas/PSITelescope_Cosmics/Telescope_test/phCalibrationFitTan_C0.dat", 0);
   //fGainCal.ReadGainCalFileExt("/Users/dhidas/PSITelescope_Cosmics/Telescope_test/phCalibrationFitTan_C1.dat", 1);
   //fGainCal.ReadGainCalFileExt("/Users/dhidas/PSITelescope_Cosmics/Telescope_test/phCalibrationFitTan_C2.dat", 2);
