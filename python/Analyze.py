c950c995f6d02426aa5eba2ee0fc7e52f3c941bd
Gregor Kasieczka <g.kasieczka@gmail.com>
2014-05-30 11:22:19 +0200

    Analyze.py

--- python/Analyze.py
+++ python/Analyze.py
@@ -6,9 +6,9 @@ Analyze multiple runs from PSI testbeam in May 2014
 
 import ROOT
 
-li_runs = [ 329, 330]
+li_runs = [ 328, 330]
 
-li_names = ["PuleHeightTrack6_ROC0_All"]
+li_names = ["PulseHeightTrack6_ROC0_All"]
 
 di_histos = {}
 
@@ -20,19 +20,19 @@ for name in li_names:
 
 for i_run, run in enumerate(li_runs):
 
-    input_rootfile_name = "../plots/000"+str(run)+"/histos.root"
+    print "Doing", run
+    input_rootfile_name = "../plots/plots/000"+str(run)+"/histos.root"
     f = ROOT.TFile.Open( input_rootfile_name )
-        
+
     for i_name, name in enumerate(li_names):
-    
+
         h = f.Get( name ).Clone()
-        h.SetFile(0)
         di_histos[name][run]=h
 
 for name in li_names:
 
-    for i_run, run in enumerate(li_runs):    
-        
+    for i_run, run in enumerate(li_runs):
+
         if i_run == 0:
             di_histos[name][i_run].Draw()
         else:
