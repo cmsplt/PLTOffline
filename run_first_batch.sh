d022e61f1a364f301b5dcbdfa494e37e594209b7
Gregor Kasieczka <g.kasieczka@gmail.com>
2014-07-29 16:00:11 +0200

    Automatically select calibration file for a given telescope

--- run_first_batch.sh
+++ run_first_batch.sh
@@ -1,14 +1,14 @@
-./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000322/mtb.bin  GKCalibrationList.txt 0 1
-./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000325/mtb.bin  GKCalibrationList.txt 0 1
-./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000327/mtb.bin  GKCalibrationList.txt 0 1
-./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000330/mtb.bin  GKCalibrationList.txt 0 1
-./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000333/mtb.bin  GKCalibrationList.txt 0 1
-./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000338/mtb.bin  GKCalibrationList.txt 0 1
-./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000340/mtb.bin  GKCalibrationList.txt 0 1
-./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000343/mtb.bin  GKCalibrationList.txt 0 1
-./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000347/mtb.bin  GKCalibrationList.txt 0 1
-./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000348/mtb.bin  GKCalibrationList.txt 0 1
-./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000350/mtb.bin  GKCalibrationList.txt 0 1
-./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000351/mtb.bin  GKCalibrationList.txt 0 1
-./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000352/mtb.bin  GKCalibrationList.txt 0 1
-./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000353/mtb.bin  GKCalibrationList.txt 0 1
+./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000322/mtb.bin  0 1
+./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000325/mtb.bin  0 1
+./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000327/mtb.bin  0 1
+./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000330/mtb.bin  0 1
+./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000333/mtb.bin  0 1
+./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000338/mtb.bin  0 1
+./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000340/mtb.bin  0 1
+./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000343/mtb.bin  0 1
+./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000347/mtb.bin  0 1
+./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000348/mtb.bin  0 1
+./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000350/mtb.bin  0 1
+./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000351/mtb.bin  0 1
+./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000352/mtb.bin  0 1
+./TestPSIBinaryFileReader /Volumes/t3/testbeam_may2014_data/bt05r000353/mtb.bin  0 1
