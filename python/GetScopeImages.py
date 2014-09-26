6dd26b79db04243a9090437320c9556a7a7a56bc
Gregor Kasieczka <g.kasieczka@gmail.com>
2014-09-26 14:17:32 +0200

    Added some documentation to GetScopeImages

--- python/GetScopeImages.py
+++ python/GetScopeImages.py
@@ -1,11 +1,26 @@
-import sys
+#!/usr/bin/env python
+
+"""
+Download screenshot from the Oscilloscope downstairs every ten minutes.
+(Used for the September 2014 PSI Diamond Testbeam)\
+"""
+
+###############################
+# Imports
+###############################
+
 import os
 import urllib2
-import urlparse
 import time
 
 
-def downloadImages(url, out_path): 
+###############################
+# Helper:
+# downloadImage
+###############################
+
+def downloadImage(url, out_path):
+    """Function to download image from an url and save it as timestamp.png"""
 
     try:
         imgData = urllib2.urlopen(url).read()
@@ -19,13 +34,15 @@ def downloadImages(url, out_path):
         
     except Exception, e:
         print str(e)
+# end of downloadImage
 
-# end of downloadImages
 
+###############################
+# Configuration
+###############################
 
 out_path = "scope_images"
 image_url = "http://129.129.140.151:81/image.png"
-
 
 # Make sure the output directory exists
 try:
@@ -33,9 +50,13 @@ try:
 except:
     pass
 
-while True:
-    
-    downloadImages(image_url, out_path)        
+
+###############################
+# Main loop
+###############################
+
+while True:    
+    downloadImage(image_url, out_path)        
     time.sleep(60. * 10) # Wait for 10 minutes
-     
+# end of main loop     
 
