3c1c765111b86a42ad0de029cc8a36e0992a3f37
griley4 <griley4@vols.utk.edu>
2014-12-03 13:37:06 +0100

    WriteAlignmentFile now prints column labels at the top of all alignment files

--- src/PLTAlignment.cc
+++ src/PLTAlignment.cc
@@ -114,7 +114,8 @@ void PLTAlignment::WriteAlignmentFile (std::string const OutFileName)
   for (std::map<int, TelescopeAlignmentStruct>::iterator it = fTelescopeMap.begin(); it != fTelescopeMap.end(); ++it) {
     int const Channel = it->first;
     TelescopeAlignmentStruct& Tele = it->second;
-
+    fprintf(Out, "#first line:  Channel,-1, Tele.GRZ, Tele.GRY, Tele.GX, Tele.GY, Tele.GZ \n");
+    fprintf(Out, "#subsequent lines:  Channel, iroc, C.LR, C.LX, C.LY, C.LZ \n");
     fprintf(Out, "\n");
     fprintf(Out, "%2i  -1        %15E      %15E  %15E  %15E  %15E\n", Channel, Tele.GRZ, Tele.GRY, Tele.GX, Tele.GY, Tele.GZ);
 
