3acd769e95e4c00e6ac76b8833f5f40c75308255
Andrés G. Delannoy <andres.delannoy@cern.ch>
2021-02-23 18:03:10 +0100

    Update plotOccupancy.py
    implemented input file as sys.argv

--- scripts/plotOccupancy.py
+++ scripts/plotOccupancy.py
@@ -3,9 +3,9 @@
 # pip install uproot # [https://github.com/scikit-hep/uproot4]
 import os, sys, re, uproot, pandas, matplotlib.pyplot
 
-def testFile(fileName='histo_occupancy.root'):
-    if not os.path.isfile(fileName):
-        os.system(f'wget https://adelannoy.com/CMS/PLT/tmp/Fill4984-Slink20160603.153652/histo_occupancy.root -O {fileName}') # ~750 kB
+def testFile(filePath='histo_occupancy.root'):
+    if not os.path.isfile(filePath):
+        os.system(f'wget https://adelannoy.com/CMS/PLT/tmp/Fill4984-Slink20160603.153652/histo_occupancy.root -O {filePath}') # ~750 kB
 
 def plotOccupancyROC(rootFile, key=f'Occupancy Ch02 ROC0;1'):
     # [https://uproot.readthedocs.io/en/latest/basic.html]
@@ -24,8 +24,12 @@ def plotOccupancyROC(rootFile, key=f'Occupancy Ch02 ROC0;1'):
     matplotlib.pyplot.close('all')
 
 def main():
-    testFile('histo_occupancy.root')
-    with uproot.open('histo_occupancy.root') as rootFile: # [https://uproot.readthedocs.io/en/latest/uproot.reading.open.html]
+    # testFile()
+    def usage(): sys.exit('usage:\n./plotOccupancy.py /path/to/histo_occupancy/root/file')
+    try: filePath = str(sys.argv[1])
+    except: usage()
+    if not os.path.isfile(filePath): usage()
+    with uproot.open(filePath) as rootFile: # [https://uproot.readthedocs.io/en/latest/uproot.reading.open.html]
         errors = pandas.Series(rootFile['Errors;1'].values()).rename(dict(zip([*range(6)],['timeOut','eventNum','nearFull','trailer','tbm','unknown'])))
         for key in [ k for k in rootFile.keys() if re.match('Occupancy Ch[\d]{2} ROC[\d];1', k) ]:
             print(f'plotting {key[:-2]}...')
