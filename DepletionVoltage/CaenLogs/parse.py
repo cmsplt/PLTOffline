6573dfaae2d5abdeb8c96c28afdcdbab50222a20
adelannoy <andres.delannoy@cern.ch>
2023-11-15 12:40:38 +0100

    added IV per month plotting function

--- DepletionVoltage/CaenLogs/parse.py
+++ DepletionVoltage/CaenLogs/parse.py
@@ -1,7 +1,9 @@
 #!/cvmfs/cms-bril.cern.ch/brilconda310/bin/python3
 
+from __future__ import annotations
 import pathlib
 
+import matplotlib.pyplot
 import pandas
 
 CHANNEL_MAP = dict(zip(range(16), [f'"CMS_PLT/PLT_H{q[0]}/PLT_H{q}/PLTHV_H{q}T{ch}"' for q in ['mN','mF','pN','pF'] for ch in [0,2,1,3]])) # https://twiki.cern.ch/twiki/bin/viewauth/CMS/PLT#PLT_Channel_Map
@@ -17,3 +19,18 @@ def parseChannel(log_file: str, ch: int) -> pandas.DataFrame:
     ch_log = ch_log.astype(float) # ch_log.apply(pandas.to_numeric)
     ch_log['Date'] = pandas.to_datetime(ch_log.Date, unit='ms', utc=True)
     return ch_log
+
+def mergeIV(year: int, ch: int) -> pandas.DataFrame:
+    vmon = parseChannel(log_file=f'{year}-vmon.csv', ch=ch)
+    imon = parseChannel(log_file=f'{year}-imon.csv', ch=ch)
+    data = pandas.merge(vmon, imon, how='outer')
+    data = data.fillna(method='ffill') # CAEN only appends to the log when there is a change to the value; thus, fill all nan by propagating from the last valid value
+    return data
+
+def plotIVperMonth(year: int, ch: int):
+    data = mergeIV(year=year, ch=ch)
+    for month in range(1, 13):
+        print(f'{year}-{month:02}')
+        month_data = data[data.Date.dt.month == month]
+        axis = month_data.plot.scatter(x='ACTUAL_VMON', y='ACTUAL_IMON', xlabel='HV (V)', ylabel='I (uA)', title=f'Ch{ch:02} current draw vs HV during {year}-{month:02}')
+        matplotlib.pyplot.savefig(fname=f'{year}-{month:02}-ch{ch:02}-IV.png')
